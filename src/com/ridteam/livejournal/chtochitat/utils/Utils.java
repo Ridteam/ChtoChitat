bd868009e2c8a778d46e6a4fc296b5d27740dc2a
Viacheslav Titov <viacheslavtitov90@gmail.com>
2013-07-12 23:45:14 +0300

    add Utils class with base methods

---
+++ src/com/ridteam/livejournal/chtochitat/utils/Utils.java
@@ -0,0 +1,238 @@
+package com.ridteam.livejournal.chtochitat.utils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.os.Build;
+import android.support.v4.app.FragmentActivity;
+import android.util.Base64;
+import android.util.Log;
+
+public class Utils
+{
+
+	public static final String TAG = "Utils";
+	private static final boolean isDebug = true;
+
+	/**
+	 * chack if internet connection is open
+	 * 
+	 * @param activity
+	 * @return true if internet connection is open
+	 */
+	public static boolean isExistConnection(FragmentActivity activity)
+	{
+		if (activity != null)
+		{
+			try
+			{
+				ConnectivityManager conMgr = (ConnectivityManager) activity.getSystemService(Context.CONNECTIVITY_SERVICE);
+				if (conMgr != null && conMgr.getActiveNetworkInfo().isConnectedOrConnecting())
+				{
+					return true;
+				}
+			}
+			catch (Exception e)
+			{
+				logError(TAG, e);
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * write exception message to log
+	 * 
+	 * @param tag
+	 * @param e
+	 */
+	public static void logError(final String tag, final Exception e)
+	{
+		if (e != null && e.getMessage() != null)
+		{
+			Log.e(tag, e.getMessage());
+		}
+	}
+
+	/**
+	 * write message of debug to log
+	 * 
+	 * @param tag
+	 * @param sDebug
+	 */
+	public static void logDebug(final String tag, final String sDebug)
+	{
+		if (isDebug)
+		{
+			Log.d(tag, sDebug);
+		}
+	}
+
+	public static boolean hasGingerbread()
+	{
+		return Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD;
+	}
+
+	public static boolean hasHoneycomb()
+	{
+		return Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB;
+	}
+
+	public static boolean hasHoneycombMR1()
+	{
+		return Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR1;
+	}
+
+	public static boolean hasJellyBean()
+	{
+		return Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN;
+	}
+
+	public static boolean hasIceCreamSandwitch()
+	{
+		return Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH;
+	}
+
+	/**
+	 * encode MD5 to String
+	 * 
+	 * @param text
+	 * @return encode text
+	 */
+	public static String encodeMD5(String text)
+	{
+		try
+		{
+			final MessageDigest md = MessageDigest.getInstance("MD5");
+			return bytesToHex(md.digest(text.getBytes()));
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			return "";
+		}
+	}
+
+	/**
+	 * decode String to MD5
+	 * 
+	 * @param text
+	 * @return decode text
+	 */
+	public static String decodeMD5(String text)
+	{
+		try
+		{
+			MessageDigest digest = java.security.MessageDigest.getInstance("MD5");
+			digest.update(text.getBytes());
+			byte messageDigest[] = digest.digest();
+			StringBuffer hexString = new StringBuffer();
+			for (int i = 0; i < messageDigest.length; i++)
+				hexString.append(Integer.toHexString(0xFF & messageDigest[i]));
+			return hexString.toString();
+
+		}
+		catch (NoSuchAlgorithmException e)
+		{
+			e.printStackTrace();
+			return "";
+		}
+	}
+
+	private static String bytesToHex(byte[] buf)
+	{
+		final StringBuilder b = new StringBuilder(buf.length * 2);
+
+		for (int i = 0; i < buf.length; i++)
+		{
+			final int cell = (int) (buf[i] & 0xFF);
+			if (cell < 16)
+			{
+				b.append("0");
+			}
+
+			b.append(Integer.toString(cell, 16));
+		}
+
+		return b.toString();
+	}
+
+	/**
+	 * encode Base64 to text
+	 * 
+	 * @param text
+	 * @return encode text
+	 */
+	public static String encodeBase64(String text)
+	{
+		byte[] data = null;
+		try
+		{
+			data = text.getBytes("UTF-8");
+		}
+		catch (UnsupportedEncodingException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return Base64.encodeToString(data, Base64.DEFAULT);
+	}
+
+	/**
+	 * decode text to Base64
+	 * 
+	 * @param text
+	 * @return decode text
+	 */
+	public static String decodeBase64(String text)
+	{
+		byte[] data = Base64.decode(text, Base64.DEFAULT);
+		try
+		{
+			return new String(data, "UTF-8");
+		}
+		catch (UnsupportedEncodingException e)
+		{
+			e.printStackTrace();
+		}
+		return "";
+	}
+
+	// TODO: test
+	public static void writeStringToFile(String text, Context context, String filename)
+	{
+		PrintStream out = null;
+		File file = new File(context.getFilesDir().getPath().toString() + File.separator + filename + ".txt");
+		if (!file.exists())
+		{
+			try
+			{
+				file.createNewFile();
+			}
+			catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+		}
+		Log.i(TAG, file.getAbsolutePath());
+		try
+		{
+			out = new PrintStream(new FileOutputStream(file.getAbsolutePath()));
+		}
+		catch (FileNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+		out.print(text);
+		out.close();
+	}
+
+}
